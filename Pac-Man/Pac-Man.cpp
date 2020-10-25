23a5e5ccbca4144464927e1b5973b32a8cd2f6c6
Shu-Huai <lvzhihe_123@qq.com>
2020-10-25 20:06:25 +0800

    add rand movement of ghost

--- Pac-Man/Pac-Man.cpp
+++ Pac-Man/Pac-Man.cpp
@@ -4,6 +4,7 @@
 #define B 1
 #define K 2
 #define P 3
+#define G 4
 using namespace std;
 class MAP
 {
@@ -26,6 +27,16 @@ public:
 	int GetPX();
 	int GetPY();
 	void SetPlayerPosition(int X, int Y);
+};
+class Ghost :public MAP
+{
+private:
+	int x, y;
+public:
+	Ghost(int X = 1, int Y = 1);
+	int GetPX();
+	int GetPY();
+	void SetGhostPosition(int X, int Y);
 };
 int MAP::map[19][19] = {
 	W, W, W, W, W, W, W, W, W, B, W, W, W, W, W, W, W, W, W,
@@ -94,6 +105,53 @@ void Player::SetPlayerPosition(int X, int Y)
 		y = tempy;
 	}
 }
+Ghost::Ghost(int X, int Y) :MAP()
+{
+	x = X;
+	y = Y;
+	map[x][y] = G;
+}
+int Ghost::GetPX()
+{
+	return x;
+}
+int Ghost::GetPY()
+{
+	return y;
+}
+void Ghost::SetGhostPosition(int X, int Y)
+{
+	int tempx = x;
+	int tempy = y;
+	x += X;
+	y += Y;
+	if (x > 18)
+	{
+		x -= 19;
+	}
+	if (y > 18)
+	{
+		y -= 19;
+	}
+	if (x < 0)
+	{
+		x += 19;
+	}
+	if (y < 0)
+	{
+		y += 19;
+	}
+	if (map[x][y] != W)
+	{
+		map[tempx][tempy] = B;
+		map[x][y] = G;
+	}
+	else
+	{
+		x = tempx;
+		y = tempy;
+	}
+}
 void MAP::drawmap()
 {
 	system("cls");
@@ -116,6 +174,10 @@ void MAP::drawmap()
 			else if (map[i][j] == 3)
 			{
 				cout << "★";
+			}
+			else if (map[i][j] == 4)
+			{
+				cout << "囧";
 			}
 		}
 		cout << endl;
@@ -140,6 +202,8 @@ int main()
 {
 	MAP first;
 	Player PL;
+	Ghost GH0(17, 1);
+	Ghost GH1(17, 17);
 	first.drawmap();
 	while (first.GetBeanNumber() != 0)
 	{
@@ -147,21 +211,29 @@ int main()
 		if (input == 'w')
 		{
 			PL.SetPlayerPosition(-1, 0);
+			GH0.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
+			GH1.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
 			first.drawmap();
 		}
 		else if (input == 'a')
 		{
 			PL.SetPlayerPosition(0, -1);
+			GH0.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
+			GH1.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
 			first.drawmap();
 		}
 		else if (input == 's')
 		{
 			PL.SetPlayerPosition(1, 0);
+			GH0.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
+			GH1.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
 			first.drawmap();
 		}
 		else if (input == 'd')
 		{
 			PL.SetPlayerPosition(0, 1);
+			GH0.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
+			GH1.SetGhostPosition(1 - (rand() % (2 + 1)), 1 - (rand() % (2 + 1)));
 			first.drawmap();
 		}
 	}
