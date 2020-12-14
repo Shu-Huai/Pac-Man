0a29e17c5b2770ec9bdb7210e896c6548105fc6e
Shu-Huai <lvzhihe123@qq.com>
2020-12-14 12:52:51 +0800

    renamed MapData.dat

--- Pac-Man/File.cpp
+++ Pac-Man/File.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 void  Write(Map& map)
 {
-	fstream binary_file("Map.dat", ios::out | ios::binary | ios::app);  // 此处省略文件是否打开失败的判断
+	fstream binary_file("MapData.dat", ios::out | ios::binary | ios::app);  // 此处省略文件是否打开失败的判断
 	binary_file.write((char*)&map, sizeof(map)); // 二进制写入的方式
 	binary_file.close();
 }
